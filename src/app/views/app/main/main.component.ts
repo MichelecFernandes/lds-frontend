8436d15d115a6dfbba1696c1d06d28ce5025300d
MichelecFernandes <michelecristinafr@gmail.com>
2024-05-16 20:49:56 -0300

    feat: adicao do estilo em main

--- src/app/views/app/main/main.component.ts
+++ src/app/views/app/main/main.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'lds-main',
   standalone: true,
   imports: [
     RouterOutlet,
-    
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule, 
+    MatExpansionModule, 
+    MatTooltipModule,
+    FontAwesomeModule
+
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
