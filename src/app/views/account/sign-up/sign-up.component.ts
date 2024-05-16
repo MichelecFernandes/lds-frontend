96915420e3ec92f0b504d4b80fb1008a2d125c66
MichelecFernandes <michelecristinafr@gmail.com>
2024-05-16 20:07:37 -0300

    feat: adicao do estilo no sign-up

--- src/app/views/account/sign-up/sign-up.component.ts
+++ src/app/views/account/sign-up/sign-up.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'lds-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
