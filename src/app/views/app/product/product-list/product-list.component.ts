6fcfd90111c4f48c6c66b40bee414600cd6c33ce
MichelecFernandes <michelecristinafr@gmail.com>
2024-05-21 21:25:56 -0300

    refactor: atualizacao dos arquivos html e css do projeto

--- src/app/views/app/product/product-list/product-list.component.ts
+++ src/app/views/app/product/product-list/product-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import * as fontawesome from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lds-product-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  fa = fontawesome;
 
 }
